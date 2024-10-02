9f6f38ab2e35c522b11f0ebf064fe9e0aa542509
Jaylen Baxter <14062951+JYLN@users.noreply.github.com>
2024-10-02 15:14:30 -0600

    :art: Setup typography and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.html"],
   theme: {
-    extend: {},
+    colors: {
+      green: "#C4F82A",
+      grey: {
+        700: "#333333",
+        800: "#1F1F1F",
+        900: "#141414",
+      },
+      white: "#FFFFFF",
+    },
+    extend: {
+      fontFamily: {
+        inter: ["Inter Variable", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
